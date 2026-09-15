d74e1b7136670d42ac42459e24f170b4becb652f
AmrRaafat <amrraafat>
2026-09-15 16:59:45 +0300

    Question 1

--- G-ASP-NET-101-C#Basics-05/Program.cs
+++ G-ASP-NET-101-C#Basics-05/Program.cs
@@ -4,6 +4,31 @@ namespace G_ASP_NET_101_C_Basics_05
     {
         static void Main(string[] args)
         {
+            #region Question 1
+
+            // This method tries to get the book price.
+            // Expected result: found price is 25.5 for Clean Code.
+            bool found = TryGetPrice("Clean Code", out double price);
+
+            if (found)
+            {
+                Console.WriteLine($"Price: {price}");
+            }
+
+            #endregion
+        }
+
+        static bool TryGetPrice(string title, out double price)
+        {
+            // This checks if the title is Clean Code.
+            if (title == "Clean Code")
+            {
+                price = 25.5;
+                return true;
+            }
+
+            price = 0;
+            return false;
         }
     }
 }
